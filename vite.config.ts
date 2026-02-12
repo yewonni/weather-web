85414f9b158459a99a9c33c01cc40e337b3121f7
ddhsl <dnjs9199@naver.com>
2026-02-12 12:14:21 +0900

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api/weather/geo": {
+        target: "https://api.openweathermap.org/geo",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/weather\/geo/, ""),
+      },
+      "/api/weather": {
+        target: "https://api.openweathermap.org/data/2.5",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/weather/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
