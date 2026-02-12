03d842740a1dd9a624324cfb61fb6db15eac7638
ddhsl <dnjs9199@naver.com>
2026-02-12 12:29:59 +0900

    feat: add app component

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "@tanstack/react-router";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main className="min-h-screen px-4 md:px-10 lg:px-30 py-10 lg:pb-12 max-w-[1200px] mx-auto">
+        <Outlet />
+      </main>
+    </QueryClientProvider>
+  );
+}
