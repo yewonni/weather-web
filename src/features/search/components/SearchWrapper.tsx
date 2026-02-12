a1e955ca926cbbca93550f78892e6b0945e2b47a
ddhsl <dnjs9199@naver.com>
2026-02-12 12:26:03 +0900

    feat: add search wrapper component

---
+++ src/features/search/components/SearchWrapper.tsx
@@ -0,0 +1,48 @@
+import SearchDropdown from "./SearchDropdown";
+
+interface SearchWrapperProps {
+  value: string;
+  onChange: (value: string) => void;
+  onFocus: () => void;
+  onBlur: () => void;
+  onSelect: (fullAddress: string) => void;
+  results: string[];
+  showDropdown: boolean;
+}
+
+export default function SearchWrapper({
+  value,
+  onChange,
+  onFocus,
+  onBlur,
+  onSelect,
+  results,
+  showDropdown,
+}: SearchWrapperProps) {
+  return (
+    <>
+      {showDropdown && (
+        <div className="fixed inset-0 bg-black/20 z-40" onClick={onBlur} />
+      )}
+
+      <div className="relative z-50">
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          onFocus={onFocus}
+          placeholder="예: 서울특별시, 강남구, 신사동"
+          className="w-full px-4 py-3 border rounded-lg bg-white"
+        />
+
+        {showDropdown && (
+          <SearchDropdown
+            results={results}
+            keyword={value}
+            onSelect={onSelect}
+          />
+        )}
+      </div>
+    </>
+  );
+}
