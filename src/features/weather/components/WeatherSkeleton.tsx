da4f57d7ecbdc2c369e89f9485a53e46e03df05d
ddhsl <dnjs9199@naver.com>
2026-02-13 21:56:50 +0900

    chore: delete animation from weather skeleton

--- src/features/weather/components/WeatherSkeleton.tsx
+++ src/features/weather/components/WeatherSkeleton.tsx
@@ -2,7 +2,7 @@ export default function WeatherSkeleton() {
   return (
     <div className="mt-6">
       {/* WeatherCard */}
-      <div className="w-full p-4 animate-pulse">
+      <div className="w-full p-4">
         <div className="h-6 bg-gray-200 rounded w-32 mb-2 mx-auto md:mx-0"></div>
         <div className="flex flex-col items-center md:flex-row md:items-center md:justify-between gap-2 mb-2">
           <div className="h-20 bg-gray-200 rounded w-24"></div>
@@ -18,7 +18,7 @@ export default function WeatherSkeleton() {
           {[...Array(8)].map((_, i) => (
             <div
               key={i}
-              className="flex flex-col items-center gap-1 min-w-[50px] animate-pulse"
+              className="flex flex-col items-center gap-1 min-w-[50px]"
             >
               <div className="h-3 bg-gray-200 rounded w-10"></div>
               <div className="h-4 bg-gray-200 rounded w-8"></div>
